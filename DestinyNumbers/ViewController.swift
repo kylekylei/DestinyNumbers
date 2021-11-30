3baecf9fee78bfc268c8b6cbeb8b07e81fb1875f
Kyle <kylei.tw@gmail.com>
2021-11-30 18:09:55 +0800

    Initial Commit

---
+++ DestinyNumbers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DestinyNumbers
+//
+//  Created by Kyle Lei on 2021/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
